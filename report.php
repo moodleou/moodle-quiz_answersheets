2b9137d520f6f11098f5d25e92d7f2d3109a69e1
Huong Nguyen <huongnv13@gmail.com>
2019-09-12 14:28:26 +0700

    Report skeleton that lists students in the quiz, and whether they have an in-progress attempt

---
+++ report.php
@@ -0,0 +1,147 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file defines the quiz answer sheets report class.
+ *
+ * @package   quiz_answersheets
+ * @copyright 2019 The Open University
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/mod/quiz/report/attemptsreport.php');
+require_once($CFG->dirroot . '/mod/quiz/report/answersheets/quiz_answersheets_options.php');
+require_once($CFG->dirroot . '/mod/quiz/report/answersheets/quiz_answersheets_settings_form.php');
+require_once($CFG->dirroot . '/mod/quiz/report/answersheets/quiz_answersheets_table.php');
+
+/**
+ * This file defines the quiz answer sheets report class.
+ *
+ * @package   quiz_answersheets
+ * @copyright 2019 The Open University
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class quiz_answersheets_report extends quiz_attempts_report {
+
+    public function display($quiz, $cm, $course) {
+        global $DB, $CFG;
+
+        list($currentgroup, $studentsjoins, $groupstudentsjoins, $allowedjoins) = $this->init(
+                'answersheets', 'quiz_answersheets_settings_form', $quiz, $cm, $course);
+
+        $options = new quiz_answersheets_options('answersheets', $quiz, $cm, $course);
+
+        if ($fromform = $this->form->get_data()) {
+            $options->process_settings_from_form($fromform);
+        } else {
+            $options->process_settings_from_params();
+        }
+
+        $this->form->set_data($options->get_initial_form_data());
+
+        // Load the required questions.
+        $questions = quiz_report_get_significant_questions($quiz);
+
+        // Prepare for downloading, if applicable.
+        $courseshortname = format_string($course->shortname, true, ['context' => context_course::instance($course->id)]);
+        $table = new quiz_answersheets_table($quiz, $this->context, $this->qmsubselect,
+                $options, $groupstudentsjoins, $studentsjoins, $questions, $options->get_url());
+        $filename = quiz_report_download_filename(get_string('answersheetsfilename', 'quiz_answersheets'),
+                $courseshortname, $quiz->name);
+        $table->is_downloading($options->download, $filename, $courseshortname . ' ' . format_string($quiz->name, true));
+        if ($table->is_downloading()) {
+            raise_memory_limit(MEMORY_EXTRA);
+        }
+
+        $this->hasgroupstudents = false;
+        if (!empty($groupstudentsjoins->joins)) {
+            $sql = "SELECT DISTINCT u.id
+                      FROM {user} u
+                           $groupstudentsjoins->joins
+                     WHERE $groupstudentsjoins->wheres";
+            $this->hasgroupstudents = $DB->record_exists_sql($sql, $groupstudentsjoins->params);
+        }
+        $hasstudents = false;
+        if (!empty($studentsjoins->joins)) {
+            $sql = "SELECT DISTINCT u.id
+                      FROM {user} u
+                           $studentsjoins->joins
+                     WHERE $studentsjoins->wheres";
+            $hasstudents = $DB->record_exists_sql($sql, $studentsjoins->params);
+        }
+        if ($options->attempts == self::ALL_WITH) {
+            // This option is only available to users who can access all groups in
+            // groups mode, so setting allowed to empty (which means all quiz attempts
+            // are accessible, is not a security problem.
+            $allowedjoins = new \core\dml\sql_join();
+        }
+
+        $this->process_actions($quiz, $cm, $currentgroup, $groupstudentsjoins, $allowedjoins, $options->get_url());
+
+        $hasquestions = quiz_has_questions($quiz->id);
+
+        // Start output.
+        if (!$table->is_downloading()) {
+            // Only print headers if not asked to download data.
+            $this->print_standard_header_and_messages($cm, $course, $quiz,
+                    $options, $currentgroup, $hasquestions, $hasstudents);
+
+            // Print the display options.
+            $this->form->display();
+        }
+
+        $hasstudents = $hasstudents && (!$currentgroup || $this->hasgroupstudents);
+        if ($hasquestions && ($hasstudents || $options->attempts == self::ALL_WITH)) {
+
+            $table->setup_sql_queries($allowedjoins);
+
+            // Define table columns.
+            $columns = [];
+            $headers = [];
+
+            if (!$table->is_downloading() && $options->checkboxcolumn) {
+                $columns[] = 'checkbox';
+                if ($CFG->branch >= 38) {
+                    // Checkbox header only available since Moodle 3.8.
+                    $headers[] = $table->checkbox_col_header();
+                } else {
+                    $headers[] = null;
+                }
+            }
+
+            $this->add_user_columns($table, $columns, $headers);
+            $this->add_state_column($columns, $headers);
+            $this->add_time_columns($columns, $headers);
+
+            $table->define_columns($columns);
+            $table->define_headers($headers);
+            $table->sortable(true, 'uniqueid');
+            $table->no_sorting('checkbox');
+
+            // Set up the table.
+            $table->define_baseurl($options->get_url());
+            $this->configure_user_columns($table);
+            $table->set_attribute('id', 'answersheets');
+            $table->collapsible(true);
+            $table->out($options->pagesize, true);
+        }
+
+        return true;
+    }
+
+}
---
+++ tests/behat/basic.feature
@@ -0,0 +1,58 @@
+@mod @mod_quiz @quiz @quiz_answersheets
+Feature: Basic use of the Answer sheets report
+  In order to generate a paper version for Quiz
+  As a teacher
+  I need to access Answer sheets report
+
+  Background: Using the Answer sheets report
+    Given the following "users" exist:
+      | username | firstname | lastname |
+      | teacher  | The       | Teacher  |
+      | student1 | Student   | One      |
+      | student2 | Student   | Two      |
+    And the following "courses" exist:
+      | fullname | shortname |
+      | Course 1 | C1        |
+    And the following "course enrolments" exist:
+      | user     | course | role           |
+      | teacher  | C1     | editingteacher |
+      | student1 | C1     | student        |
+      | student2 | C1     | student        |
+    And the following "question categories" exist:
+      | contextlevel | reference | name           |
+      | Course       | C1        | Test questions |
+    And the following "activities" exist:
+      | activity   | name   | intro              | course | idnumber |
+      | quiz       | Quiz 1 | Quiz 1 description | C1     | quiz1    |
+    And the following "questions" exist:
+      | questioncategory | qtype     | name | questiontext   |
+      | Test questions   | truefalse | TF1  | First question |
+    And quiz "Quiz 1" contains the following questions:
+      | question | page |
+      | TF1      | 1    |
+
+  @javascript
+  Scenario: Answer sheets report works when there are no attempts
+    Given I log in as "teacher"
+    And I am on "Course 1" course homepage
+    And I follow "Quiz 1"
+    When I navigate to "Results > Answer sheets" in current page administration
+    Then I should see "Attempts: 0"
+    And I should see "Nothing to display"
+    And I log out
+
+  @javascript
+  Scenario: Answer sheets report works when there are attempts
+    Given user "student1" has attempted "Quiz 1" with responses:
+      | slot | response |
+      | 1    | True     |
+    And I log in as "teacher"
+    And I am on "Course 1" course homepage
+    And I follow "Quiz 1"
+    When I navigate to "Results > Answer sheets" in current page administration
+    Then I should see "Attempts: 1"
+    And I should see "Student One"
+    And I should not see "Student Two"
+    And I set the field "Attempts from" to "enrolled users who have, or have not, attempted the quiz"
+    And I press "Show report"
+    And I should see "Student Two"
