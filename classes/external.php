7764a0060d1bb57ef516fd00d9f7b68afca562ec
Tran Bao Lam <tranbaolam6693@gmail.com>
2019-10-15 16:11:34 +0700

    Allow a staff member can create an attempt for a student who does not have one in-progress

---
+++ classes/external.php
@@ -0,0 +1,157 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * External Quiz Answersheets API
+ *
+ * @package   quiz_answersheets
+ * @copyright 2019 The Open University
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use quiz_answersheets\utils;
+
+defined('MOODLE_INTERNAL') || die;
+
+require_once("$CFG->libdir/externallib.php");
+require_once($CFG->dirroot . '/mod/quiz/locallib.php');
+
+class quiz_answersheets_external extends external_api {
+
+    /**
+     * Describes the parameters for create_attempt.
+     *
+     * @return external_function_parameters
+     */
+    public static function create_attempt_parameters() {
+        return new external_function_parameters([
+                'quizid' => new external_value(PARAM_INT, 'Quiz Id'),
+                'userid' => new external_value(PARAM_INT, 'User Id')
+        ]);
+    }
+
+    /**
+     * Describes the create_attempt return value.
+     *
+     * @return external_single_structure
+     */
+    public static function create_attempt_returns() {
+        return new external_single_structure([
+                'success' => new external_value(PARAM_BOOL, 'success: true/false'),
+                'message' => new external_value(PARAM_TEXT, 'response message'),
+                'id' => new external_value(PARAM_INT, 'ID of new attempt if success.')
+        ]);
+    }
+
+    /**
+     * Create Attempt API
+     *
+     * @param int $quizid
+     * @param int $userid
+     * @return array
+     */
+    public static function create_attempt($quizid, $userid) {
+        $message = '';
+
+        $params = self::validate_parameters(self::create_attempt_parameters(), [
+                'quizid' => $quizid,
+                'userid' => $userid
+        ]);
+
+        list($course, $cm) = get_course_and_cm_from_instance($params['quizid'], 'quiz');
+        $context = context_module::instance($cm->id);
+        self::validate_context($context);
+
+        $quizobj = quiz::create($params['quizid'], $params['userid']);
+
+        // Check questions.
+        if (!$quizobj->has_questions()) {
+            throw new moodle_quiz_exception($quizobj, 'noquestionsfound');
+        }
+
+        $attempts = quiz_get_user_attempts($params['quizid'], $params['userid'], 'all');
+
+        if (!utils::can_create_attempt($quizobj, $attempts)) {
+            throw new moodle_exception('webservicecannotcreateattempts', 'quiz_answersheets');
+        }
+
+        $attemptnumber = count($attempts);
+        $lastattempt = array_pop($attempts);
+        /** @todo MDL-66633 When we move to Moodle 3.8, use quiz_prepare_and_start_new_attempt in mod/quiz/locallib.php. */
+        $attempt = static::quiz_prepare_and_start_new_attempt($quizobj, $attemptnumber + 1, $lastattempt, false, [], [],
+                $params['userid']);
+        $response = ['success' => true, 'message' => $message, 'id' => $attempt->id];
+
+        return $response;
+    }
+
+    /**
+     * Prepare and start a new attempt deleting the previous preview attempts.
+     * @todo MDL-66633 When we move to Moodle 3.8, use quiz_prepare_and_start_new_attempt in mod/quiz/locallib.php.
+     *
+     * @param quiz $quizobj quiz object
+     * @param int $attemptnumber the attempt number
+     * @param object $lastattempt last attempt object
+     * @param bool $offlineattempt whether is an offline attempt or not
+     * @param array $forcedrandomquestions slot number => question id. Used for random questions,
+     *      to force the choice of a particular actual question. Intended for testing purposes only.
+     * @param array $forcedvariants slot number => variant. Used for questions with variants,
+     *      to force the choice of a particular variant. Intended for testing purposes only.
+     * @param int $userid Specific user id to create an attempt for that user, null for current logged in user
+     * @return object the new attempt
+     * @since  Moodle 3.1
+     */
+    public static function quiz_prepare_and_start_new_attempt(quiz $quizobj, $attemptnumber, $lastattempt,
+            $offlineattempt = false, $forcedrandomquestions = [], $forcedvariants = [], $userid = null) {
+        global $DB, $USER;
+
+        if ($userid === null) {
+            $userid = $USER->id;
+            $ispreviewuser = $quizobj->is_preview_user();
+        } else {
+            $ispreviewuser = has_capability('mod/quiz:preview', $quizobj->get_context(), $userid);
+        }
+        // Delete any previous preview attempts belonging to this user.
+        quiz_delete_previews($quizobj->get_quiz(), $userid);
+
+        $quba = question_engine::make_questions_usage_by_activity('mod_quiz', $quizobj->get_context());
+        $quba->set_preferred_behaviour($quizobj->get_quiz()->preferredbehaviour);
+
+        // Create the new attempt and initialize the question sessions
+        $timenow = time(); // Update time now, in case the server is running really slowly.
+        $attempt = quiz_create_attempt($quizobj, $attemptnumber, $lastattempt, $timenow, $ispreviewuser, $userid);
+
+        if (!($quizobj->get_quiz()->attemptonlast && $lastattempt)) {
+            $attempt = quiz_start_new_attempt($quizobj, $quba, $attempt, $attemptnumber, $timenow,
+                    $forcedrandomquestions, $forcedvariants);
+        } else {
+            $attempt = quiz_start_attempt_built_on_last($quba, $attempt, $lastattempt);
+        }
+
+        $transaction = $DB->start_delegated_transaction();
+
+        // Init the timemodifiedoffline for offline attempts.
+        if ($offlineattempt) {
+            $attempt->timemodifiedoffline = $attempt->timemodified;
+        }
+        $attempt = quiz_attempt_save_started($quizobj, $quba, $attempt);
+
+        $transaction->allow_commit();
+
+        return $attempt;
+    }
+
+}
---
+++ tests/behat/create_attempt.feature
@@ -0,0 +1,96 @@
+@mod @mod_quiz @quiz @quiz_answersheets @quiz_answersheets_create_attempt
+Feature: Basic use of the Answer sheets report
+  In order to create new user's attempt for Quiz
+  As a teacher
+  I need to access Answer sheets report and see Create Attempt column
+
+  Background: Using the Answer sheets report
+    Given the following "users" exist:
+      | username | firstname | lastname |
+      | teacher  | The       | Teacher  |
+      | student1 | Student   | One      |
+      | student2 | Student   | Two      |
+      | student3 | Student   | Three    |
+      | student4 | Student   | Four     |
+    And the following "courses" exist:
+      | fullname | shortname |
+      | Course 1 | C1        |
+    And the following "course enrolments" exist:
+      | user     | course | role           |
+      | teacher  | C1     | editingteacher |
+      | student1 | C1     | student        |
+      | student2 | C1     | student        |
+      | student3 | C1     | student        |
+      | student4 | C1     | student        |
+    And the following "question categories" exist:
+      | contextlevel | reference | name           |
+      | Course       | C1        | Test questions |
+    And the following "activities" exist:
+      | activity | name   | intro              | course | idnumber | attempts |
+      | quiz     | Quiz 1 | Quiz 1 description | C1     | quiz1    | 2        |
+    And the following "questions" exist:
+      | questioncategory | qtype     | name | questiontext   |
+      | Test questions   | truefalse | TF1  | First question |
+    And quiz "Quiz 1" contains the following questions:
+      | question | page |
+      | TF1      | 1    |
+
+  @javascript
+  Scenario: "Create attempt" button should be visible when student does not have any attempts
+    Given I log in as "teacher"
+    And I am on "Course 1" course homepage
+    And I follow "Quiz 1"
+    When I navigate to "Results > Answer sheets" in current page administration
+    Then I set the field "Attempts from" to "enrolled users who have, or have not, attempted the quiz"
+    And I click on "Show report" "button"
+    Then "Create Attempt" "button" should exist in the "Student One" "table_row"
+
+  @javascript
+  Scenario: "Create attempt" button should be visible when student does have finished attempts but less than max attempts .
+    Given user "student2" has started an attempt at quiz "Quiz 1"
+    And user "student2" has finished an attempt at quiz "Quiz 1"
+    When I log in as "teacher"
+    And I am on "Course 1" course homepage
+    And I follow "Quiz 1"
+    When I navigate to "Results > Answer sheets" in current page administration
+    Then I set the field "Attempts from" to "enrolled users who have, or have not, attempted the quiz"
+    And I click on "Show report" "button"
+    Then "Create Attempt" "button" should exist in the "Student Two" "table_row"
+
+  @javascript
+  Scenario: "Create attempt" button should not be visible when student have "in progress" attempt.
+    Given user "student3" has started an attempt at quiz "Quiz 1"
+    When I log in as "teacher"
+    Then I am on "Course 1" course homepage
+    And I follow "Quiz 1"
+    When I navigate to "Results > Answer sheets" in current page administration
+    Then I set the field "Attempts from" to "enrolled users who have, or have not, attempted the quiz"
+    And I click on "Show report" "button"
+    Then "Create Attempt" "button" should not exist in the "Student Three" "table_row"
+
+  @javascript
+  Scenario: "Create attempt" button should show dialog and create new attempt with highlight
+    Given I log in as "teacher"
+    And I am on "Course 1" course homepage
+    And I follow "Quiz 1"
+    When I navigate to "Results > Answer sheets" in current page administration
+    Then I set the field "Attempts from" to "enrolled users who have, or have not, attempted the quiz"
+    And I click on "Show report" "button"
+    And I click on "Create Attempt" "button" in the "Student One" "table_row"
+    Then I should see "Are you sure you want to create a quiz attempt for Student One (student1@example.com)?" in the ".modal .modal-body" "css_element"
+    When I click on "Create" "button" in the ".modal.show" "css_element"
+    Then I should see "In progress" in the "Student One" "table_row"
+    And ".lastchanged" "css_element" should exist in the "Student One" "table_row"
+
+  @javascript
+  Scenario: Cancel button in the dialogue really cancels
+    Given I log in as "teacher"
+    And I am on "Course 1" course homepage
+    And I follow "Quiz 1"
+    When I navigate to "Results > Answer sheets" in current page administration
+    Then I set the field "Attempts from" to "enrolled users who have, or have not, attempted the quiz"
+    And I click on "Show report" "button"
+    And I click on "Create Attempt" "button" in the "Student One" "table_row"
+    Then I should see "Are you sure you want to create a quiz attempt for Student One (student1@example.com)?" in the ".modal .modal-body" "css_element"
+    When I click on "Cancel" "button" in the ".modal.show" "css_element"
+    Then I should see "Create Attempt" in the "Student One" "table_row"
